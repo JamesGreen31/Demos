f91a4b0c05e4ff1cbf6f5f2434177eca140d8395
JGreen <72313560+JamesGreen31@users.noreply.github.com>
2026-02-08 16:21:59 -0500

    Enforce efficient WASM algorithm in Part 2 timed race

--- wasm/fibonacci/src/lib.rs
+++ wasm/fibonacci/src/lib.rs
@@ -6,3 +6,8 @@ pub extern "C" fn fib(n: u32) -> u32 {
         fib(n - 1) + fib(n - 2)
     }
 }
+
+#[no_mangle]
+pub extern "C" fn fib_next(prev: u32, curr: u32) -> u32 {
+    prev.wrapping_add(curr)
+}
